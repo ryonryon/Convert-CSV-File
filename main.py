7ba550d11c742e635c016eea2b3b6b8c7927ac37
ryo-hskw <ryo.hskw0812@gmail.com>
2019-06-16 03:32:25 +0900

    fix cmd arg

--- main.py
+++ main.py
@@ -4,35 +4,38 @@ import openpyxl
 import pandas as pd
 
 import functor
-import settings
+import env
 
 
 def main():
-    base_name = sys.argv[1]
-    dir_name = settings.DIR_NAME
-    print(dir_name)
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-    abs_name = dir_name + base_name
+    dir_name = env.DIR_NAME
     acc_list = []
     gyr_list = []
+    csv_expand = ".csv"
+    xlsx_expand = ".xlsx"
 
-    with open(abs_name, "r") as f:
-        reader = csv.reader(f)
-        for row in reader:
-            if row[0] == '1':
-                gyr_list.append(row)
-            elif row[0] == '0':
-                acc_list.append(row)
-            else:
-                pass
+    try:
+        with open(dir_name + input_file + csv_expand, "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0] == '1':
+                    gyr_list.append(row)
+                elif row[0] == '0':
+                    acc_list.append(row)
+                else:
+                    pass
+    except FileNotFoundError:
+        print("File does not exist!")
 
     float_acc_list = functor.convert_list(float, acc_list)
     float_gyr_list = functor.convert_list(float, gyr_list)
 
-    output_name = sys.argv[2]
     df = pd.DataFrame(float_acc_list)
     df2 = pd.DataFrame(float_gyr_list)
-    with pd.ExcelWriter(dir_name + output_name) as w:
+    with pd.ExcelWriter(dir_name + output_file + xlsx_expand) as w:
         df.to_excel(w, sheet_name='sheet1')
         df2.to_excel(w, sheet_name='sheet2')
 
